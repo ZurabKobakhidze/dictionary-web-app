bb85782798142cccdadafb5f8dac102f3bcbef02
unknown <zukakobakhidze@gmail.com>
2023-06-05 20:50:06 +0400

    feat: add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'purple-custom': '#8F19E8'
+        "purple-custom": "#8F19E8",
+        "meaning-color": "#757575",
+        "keyboard-color": "#2D2D2D",
       },
       spacing: {
-        '5px': '5px'
+        "5px": "5px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
