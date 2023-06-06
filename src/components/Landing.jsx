b97b67f7752104fa720b4c2e313bfc526ce061aa
unknown <zukakobakhidze@gmail.com>
2023-06-06 19:02:16 +0400

    fix: function

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,20 +9,19 @@ import wikiIcon from "../assets/icon-new-window.svg";
 import axios from "axios";
 
 function Landing() {
-  const [checked, setChecked] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [information, setInformation] = useState(null);
+ 
+  
 
-  const getInformation = () => {
-    axios
-      .get(`https://dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
-      .then((response) => {
-        setInformation(response.data[0]);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  const getInformation = async () => {
+    try {
+        const response = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/school");
+        console.log(response.data);
+
+    } catch (error) {}
+      
   };
+
+  getInformation ();
 
   return (
     <>
@@ -39,15 +38,15 @@ function Landing() {
             <img src={arrowDown} alt="" className="w-[12px]" />
             <div className="bg-gray-200 w-px h-8"></div>
             <input
+              class="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
               type="checkbox"
               role="switch"
               id="flexSwitchChecked"
-              checked={checked}
-              onChange={(e) => setChecked(e.target.checked)}
+              checked
             />
             <label
-              className="inline-block pl-[0.15rem] hover:cursor-pointer"
-              htmlFor="flexSwitchChecked"
+              class="inline-block pl-[0.15rem] hover:cursor-pointer"
+              for="flexSwitchChecked"
             ></label>
             <img src={moon} alt="" className="w-[20px]" />
 
@@ -55,18 +54,14 @@ function Landing() {
           </div>
         </div>
         <div id="container1" className="mt-6">
-          <form
-            className="w-full flex justify-center"
-            onSubmit={getInformation}
-          >
-            <div className="w-full">
-              <div>
+          <form class="w-full flex justify-center">
+            <div class="w-full">
+              <div class="">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-keyboard-color leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-keyboard-color leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="searchBar"
                   type="text"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
+                  value="word"
                 />
               </div>
             </div>
@@ -74,18 +69,12 @@ function Landing() {
           </form>
           <div className="flex items-center justify-between mt-6">
             <div>
-              {information && (
-                <>
-                  <h1 className="font-inter text-4xl leading-10 font-[700] text-keyboard-color">
-                    {information.word}
-                  </h1>
-                  {information.phonetics && (
-                    <h2 className="font-inter font-[400] text-base leading-6 text-purple-custom mt-[8px]">
-                      {information.phonetics[0].text}
-                    </h2>
-                  )}
-                </>
-              )}
+              <h1 className="font-inter  text-4xl leading-10 font-[700] text-keyboard-color">
+                keyboard
+              </h1>
+              <h2 className="font-inter font-[400] text-base leading-6 text-purple-custom mt-[8px]">
+                /ˈkiːbɔːd/
+              </h2>
             </div>
             <img src={playIcon} alt="" className="w-[48px]" />
           </div>
